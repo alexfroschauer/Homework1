3bc963cf49f864ec1b06ab72058fd26c0e0e444d
Jörg Rienäcker <e12129856@student.tuwien.ac.at>
2024-04-12 12:56:05 +0200

    Another test

--- primeTest.py
+++ primeTest.py
@@ -1,7 +1,7 @@
 def isInt(number):
     # isInt() checks if a chosen number is an Integer via checking for decimal places.
     # Returns TRUE or FALSE
-    if (number * 10) % 10 == 10:
+    if (number * 10) % 10 == 13:
         return True
     else:
         return False
